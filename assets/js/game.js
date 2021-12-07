b94855b31f70b607f5adcc041df0acfd7c854986
Hunter Nelson <huntknelson@gmail.com>
2021-12-07 09:02:28 -0600

    began to add array and loop enemy enemy names element

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,45 +1,54 @@
-var playerName = window.prompt("What is your robot's name?");
+var playerName = window.prompt("What is your Model's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 // you can log mutliple values at onces like this
 // console.log(playerName, playerHealth, playerAttack)
-
-var enemyName = "Evilbot";
+// var enemyName = "Hans"
 var enemyHealth = 50;
 var enemyAttack = 12;
+
+// Enemy Robots
+var enemyNames = ["Bob", "Glorious Amy", "Sweet Jimmy", "Beautiful Hans"];
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+// console.log(enemyNames[3]);
+// var enemy1 = "Boborto";
+// var enemy2 = "Alfonso";
+// var enemy3 = "Sweet Bill";
 
 //Fight or skip prompt
 var fight = function() {
     // Alert players that they are starting the round
-    window.alert("READY TO TUSSLE?");
+    window.alert("READY FOR THE POSE OFF?");
     //Fight or Skip prompt
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP? Enter 'FIGHT' or 'SKIP' to choose!")
+    var promptFight = window.prompt("Would you like to POSE or SKIP? Enter 'POSE' or 'SKIP' to choose!")
     //Su// if player choses to fight, then fight
-if (promptFight === "fight" || promptFight === "FIGHT") {
+if (promptFight === "pose" || promptFight === "POSE") {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
     console.log(
-      playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+      playerName + " flexed their massive biceps at " + enemyName + ". " + enemyName + " is knocked back by the raw power, they have " + enemyHealth + " energy remaining."
     );
     // check enemy's health
     if (enemyHealth <= 0) {
-      window.alert(enemyName + " has died!");
+      window.alert(enemyName + " couldn't handle your awesome posing!");
     } else {
-      window.alert(enemyName + " still has " + enemyHealth + " health left.");
+      window.alert(enemyName + " still has " + enemyHealth + " energy left.");
     }
   
     // remove player's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
     console.log(
-      enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+      enemyName + " POSED HARD AGAINST " + playerName + ". " + playerName + " now has " + playerHealth + " energy remaining."
     );
   
     // check player's health
     if (playerHealth <= 0) {
-      window.alert(playerName + " has died!");
+      window.alert(playerName + " has fainted from" + enemyName+ "'s awesome posing!");
     } else {
-      window.alert(playerName + " still has " + playerHealth + " health left.");
+      window.alert(playerName + " still has " + playerHealth + " energy left.");
     }
     // if player choses to skip
   } 
@@ -48,7 +57,7 @@ else if (promptFight === "skip" || promptFight === "SKIP") {
     var confirmSkip = window.confirm("You sure about that buddy?");
     //if yes(true), leave fight
     if (confirmSkip) {
-        window.alert(playerName + " has decided to skip this fight. COWARD!");
+        window.alert(playerName + " has decided to skip this pose off. COWARD!");
         //subtract money from playerMoney for skipping
         playerMoney = playerMoney - 2;
     }
@@ -63,4 +72,14 @@ else if (promptFight === "skip" || promptFight === "SKIP") {
   }
   };
 
-fight()
+// fight()
+
+// Game States
+// Win - Player has defeated all enemy-robots
+//  *Fight all enemy-robots
+//  *Defeat all enemy-robots
+// "Lose" - Player robot's health is zero or less
+
+for(var i = 0; i < enemyNames.length; i++) {
+  console.log(enemyNames[i]);
+}
